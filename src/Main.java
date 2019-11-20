5878f8de1a1a4882dc6a53bfa815ae50ffeff435
Яна Смольникова <yanasmolnikova@MacBook-Pro-Yana.local>
2019-11-20 23:14:02 +0300

    implements undo

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Stringer stringer = new Stringer("Hello");
         stringer.append(" World");
-
+        stringer.undo();
+        stringer.undo();
+        stringer.undo();
         System.out.println(stringer.toString());
     }
 }
--- src/Stringer.java
+++ src/Stringer.java
@@ -1,12 +1,25 @@
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class Stringer implements Comparable<StringBuilder> {
-    StringBuilder sb;
-    private ArrayList<Stringer> history = new ArrayList<>();
+    private StringBuilder sb;   // delegation StringBuilder's methods
+    private Stack<StringBuilder> history = new Stack<>();   // history stack
 
     public Stringer(String str) {
-        this.sb = new StringBuilder(str);
-        this.history.add(this);
+        this.sb = new StringBuilder(str);   // create StringBuilder object
+        this.updateHistory("");     // initial history value
+    }
+
+    private void updateHistory(String str) {
+        StringBuilder state = new StringBuilder(str);
+        this.history.push(state);
+    }
+
+    private StringBuilder getPrevious() {
+        if (this.history.isEmpty()) {
+            return new StringBuilder("");
+        }
+
+        return this.history.pop();  // get last history action
     }
 
     public int compareTo(StringBuilder another) {
@@ -14,155 +27,182 @@ public class Stringer implements Comparable<StringBuilder> {
     }
 
     public Stringer undo() {
-        int lastIndex = this.history.size() - 1;
+        StringBuilder previous = this.getPrevious();
 
-        Stringer previous = this.history.get(lastIndex);
-        this.history.remove(lastIndex);
+        this.sb = new StringBuilder(previous);
 
-        String previousString = previous.toString();
-        this.sb = new StringBuilder(previousString);
+        return this;    // pre-last history action
     }
 
     public Stringer append(Object obj) {
+        this.updateHistory(this.sb.toString());     // save old version
         return this.append(String.valueOf(obj));
     }
 
     public Stringer append(String str) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(str);
         return this;
     }
 
     public Stringer append(StringBuffer sb) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(sb);
         return this;
     }
 
     public Stringer append(CharSequence s) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(s);
         return this;
     }
 
     public Stringer append(CharSequence s, int start, int end) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(s, start, end);
         return this;
     }
 
     public Stringer append(char[] str) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(str);
         return this;
     }
 
     public Stringer append(char[] str, int offset, int len) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(str, offset, len);
         return this;
     }
 
     public Stringer append(boolean b) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(b);
         return this;
     }
 
     public Stringer append(char c) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(c);
         return this;
     }
 
     public Stringer append(int i) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(i);
         return this;
     }
 
     public Stringer append(long lng) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(lng);
         return this;
     }
 
     public Stringer append(float f) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(f);
         return this;
     }
 
     public Stringer append(double d) {
+        this.updateHistory(this.sb.toString());
         this.sb.append(d);
         return this;
     }
 
     public Stringer appendCodePoint(int codePoint) {
+        this.updateHistory(this.sb.toString());
         this.sb.appendCodePoint(codePoint);
         return this;
     }
 
     public Stringer delete(int start, int end) {
+        this.updateHistory(this.sb.toString());
         this.sb.delete(start, end);
         return this;
     }
 
     public Stringer deleteCharAt(int index) {
+        this.updateHistory(this.sb.toString());
         this.sb.deleteCharAt(index);
         return this;
     }
 
     public Stringer replace(int start, int end, String str) {
+        this.updateHistory(this.sb.toString());
         this.sb.replace(start, end, str);
         return this;
     }
 
     public Stringer insert(int index, char[] str, int offset, int len) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(index, str, offset, len);
         return this;
     }
 
     public Stringer insert(int offset, Object obj) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, obj);
         return this;
     }
 
     public Stringer insert(int offset, String str) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, str);
         return this;
     }
 
     public Stringer insert(int offset, char[] str) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, str);
         return this;
     }
 
     public Stringer insert(int dstOffset, CharSequence s) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(dstOffset, s);
         return this;
     }
 
     public Stringer insert(int dstOffset, CharSequence s, int start, int end) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(dstOffset, s, start, end);
         return this;
     }
 
     public Stringer insert(int offset, boolean b) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, b);
         return this;
     }
 
     public Stringer insert(int offset, char c) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, c);
         return this;
     }
 
     public Stringer insert(int offset, int i) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, i);
         return this;
     }
 
     public Stringer insert(int offset, long l) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, l);
         return this;
     }
 
     public Stringer insert(int offset, float f) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, f);
         return this;
     }
 
     public Stringer insert(int offset, double d) {
+        this.updateHistory(this.sb.toString());
         this.sb.insert(offset, d);
         return this;
     }
@@ -184,6 +224,7 @@ public class Stringer implements Comparable<StringBuilder> {
     }
 
     public Stringer reverse() {
+        this.updateHistory(this.sb.toString());
         this.sb.reverse();
         return this;
     }
